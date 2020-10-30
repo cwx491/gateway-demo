98346df5d9e2df5131349f135150228be2aeee72
cwx491 <604298674@qq.com>
2020-10-30 11:11:00 +0800

    create

---
+++ src/test/java/com/cc/gatewaydemo/GatewayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.gatewaydemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
